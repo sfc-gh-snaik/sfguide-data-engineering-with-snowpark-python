79252eaecf1dad362f6510f4b89b9657258927a0
sfc-gh-snaik <shivali.naik@snowflake.com>
2024-08-12 19:06:34 -0700

    Create test.sql

---
+++ steps/test.sql
@@ -0,0 +1,15 @@
+USE WAREHOUSE SNAIK;
+
+--Simple Materialized View
+CREATE OR REPLACE TABLE inventory (product_ID INTEGER, wholesale_price FLOAT,
+  description VARCHAR);
+    
+CREATE OR REPLACE MATERIALIZED VIEW mv1 AS
+  SELECT product_ID, SUM(wholesale_price)sum_wholesale_price FROM inventory group by 1;
+
+INSERT INTO inventory
+SELECT UNIFORM(10000,99999,ABS(RANDOM())) AS product_ID, UNIFORM(111111111,999999999,ABS(RANDOM())) AS wholesale_price, RANDSTR(4,RANDOM()) AS DEPT 
+FROM TABLE(GENERATOR(ROWCOUNT => 20000000));
+
+--Query from MV
+select * from mv1;
